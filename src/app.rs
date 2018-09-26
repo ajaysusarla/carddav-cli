83a31b0b36df2bfe55fff4bafb1a5f67e046fc29
Partha Susarla <mail@spartha.org>
2018-09-26 15:43:22 +1000

    Use CLAP for command line parsing.

---
+++ src/app.rs
@@ -0,0 +1,88 @@
+use std::collections::HashSet;
+use std::env;
+use std::path::Path;
+use std::str::FromStr;
+
+use atty::{self, Stream};
+
+use clap::{App as ClapApp, AppSettings, Arg, ArgGroup, ArgMatches, SubCommand};
+
+use console::Term;
+
+#[cfg(windows)]
+use ansi_term;
+
+use errors::*;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum PagingMode {
+    Always,
+    QuitIfOneScreen,
+    Never,
+}
+
+#[derive(Clone)]
+pub struct Config<'a> {
+    /// The character width of the terminal
+    pub term_width: usize,
+
+    /// Pager or STDOUT
+    pub paging_mode: PagingMode,
+
+    /// User name
+    pub username: String,
+
+    /// Password
+    pub password: String,
+}
+
+fn is_truecolor_terminal() -> bool {
+    env::var("COLORTERM")
+        .map(|colorterm| colorterm == "truecolor" || colorterm == "24bit")
+        .unwrap_or(false)
+}
+
+pub struct App {
+    pub matches: ArgMatches<'static>,
+    interactive_output: bool,
+}
+
+impl App {
+    pub fn new() -> Self {
+        #[cfg(windows)]
+        let _ = ansi_term::enable_ansi_support();
+
+        let interactive_output = atty::is(Stream::Stdout);
+
+        App {
+            matches: Self::matches(interactive_output),
+            interactive_output,
+        }
+    }
+
+    fn matches(interactive_output: bool) -> ArgMatches<'static> {
+        let clap_color_setting = if interactive_output {
+            AppSettings::ColoredHelp
+        } else {
+            AppSettings::ColorNever
+        };
+
+        ClapApp::new(crate_name!())
+            .version(crate_version!())
+            .global_setting(clap_color_setting)
+            .max_term_width(90)
+            .about(
+                "A CLI based carddav client.\n\n\
+                 Use '--help' instead of '-h' to see a more detailed version of the help text.",
+            ).long_about("A CLI based carddav client.")
+            .arg(
+                Arg::with_name("URL")
+                    .help("URL of the carddav server.")
+                    .long_help("URL of the carrdav server")
+                    .multiple(false)
+                    .empty_values(false),
+            )
+            .arg(
+            )
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,73 @@
+// Limit `error_chain!`s recursion depth
+#![recursion_limit = "1024"]
+
+#[macro_use]
+extern crate error_chain;
+
+#[macro_use]
+extern crate clap;
+
+extern crate ansi_term;
+extern crate atty;
+extern crate console;
+extern crate syntect;
+
+mod app;
+
+use std::collections::HashSet;
+use std::io;
+use std::io::Write;
+use std::process;
+
+use ansi_term::Colour::Green;
+use ansi_term::Style;
+
+use app::{App, Config};
+
+mod errors {
+    error_chain! {
+        foreign_links {
+            Clap(::clap::Error);
+            Io(::std::io::Error);
+            SyntectError(::syntect::LoadingError);
+            ParseIntError(::std::num::ParseIntError);
+        }
+    }
+
+    pub fn handle_error(error: &Error) {
+        match error {
+            &Error(ErrorKind::Io(ref io_error), _)
+                if io_error.kind() == super::io::ErrorKind::BrokenPipe =>
+            {
+                super::process::exit(0);
+            }
+            _ => {
+                use ansi_term::Colour::Red;
+                eprintln!("{}: {}", Red.paint("[carddav-cli error]"), error);
+            }
+        };
+    }
+}
+
+use errors::*;
+
+fn run() -> Result<bool> {
+}
+
 fn main() {
-    println!("Hello, world!");
+    let result = run();
+
+    match result {
+        Err(error) => {
+            handle_error(&error);
+            process::exit(1);
+        }
+        Ok(false) => {
+            process::exit(1);
+        }
+        Ok(true) => {
+            process::exit(0);
+        }
+    }
+
 }
